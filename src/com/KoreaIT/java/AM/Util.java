e3a2c25d1f9dfdc6ea54e55984bdbc63fe820948
juohseo <wndh1113@gmail.com>
2022-07-21 07:32:30 +0900

    게시물 작성시 작성 날짜와 시간 저장

---
+++ src/com/KoreaIT/java/AM/Util.java
@@ -0,0 +1,16 @@
+package com.KoreaIT.java.AM;
+
+import java.text.*;
+import java.util.*;
+
+public class Util {
+	// 현재 날짜 시간
+	public static String getNowDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date time = new Date();
+
+		return format.format(time);
+	}
+
+}
